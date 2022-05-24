e640aa0ea7a487ce732baf5842bfadeb60a86b82
checkmyprojects <notnow.plz+github@gmail.com>
2022-05-24 09:45:51 +0200

    added class and refactor

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../models/Todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,29 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class TodoListComponent implements OnInit {
 
   idForTodo: number = 3;
-  todoTitle: string = '';
-  todos = [
-    {
-    'id': 1,
-    'name': 'first one',
-    'completed': true
-    },
-    {
-    'id': 2,
-    'name': 'second one',
-    'completed': false
-    }];
+  inputTodo: string = '';
+  // todos = [
+  //   {
+  //   'id': 1,
+  //   'name': 'first one',
+  //   'completed': true
+  //   },
+  //   {
+  //   'id': 2,
+  //   'name': 'second one',
+  //   'completed': false
+  //   }];
   
+  todos: Todo[];
+
   addTodo() :void{
-    if(this.todoTitle.trim().length === 0){
+    if(this.inputTodo.trim().length === 0){
       return;
     }
     this.todos.push({
       'id': this.idForTodo,
-      'name': this.todoTitle,
+      'name': this.inputTodo,
       'completed': false});
 
-    this.todoTitle = '';
+    this.inputTodo = '';
     this.idForTodo++;
   }
 
@@ -38,12 +41,34 @@ export class TodoListComponent implements OnInit {
 
 
   deleteTodo(id:number) :void{
-    this.todos = this.todos.filter(todo => todo.id != id);
+    // this.todos = this.todos.filter(todo => todo.id != id);
+    this.todos = this.todos.filter((value, idx) => idx !== id);
   }
+
+  toggleDone(id:number) :void{
+    this.todos.map((value, idx) => {
+      if(idx === id) value.completed = !value.completed;
+
+      return value;
+    })
+  }
+
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.todos = [
+      {
+        id:1,
+        name: "First Todo",
+        completed: false
+      },
+      {
+        id:2,
+        name: "Second Todo",
+        completed: true
+      }
+
+    ];
   }
 
 }
